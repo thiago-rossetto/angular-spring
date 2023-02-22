258200669044e6e764112b5d039b3fbcfc1bb835
Thiago Bessa Rossetto <thiago.rossetto@gpnet.com.br>
2023-02-22 17:49:31 -0300

    Implementando tooltip em botoes

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CoursesListComponent } from './components/courses-list/courses-list.component';
 import { ModalCoursesComponent } from './components/modal-courses/modal-courses.component';
@@ -17,7 +18,8 @@ import { CoursesRoutingModule } from './courses-routing.module';
     CommonModule,
     ReactiveFormsModule,
     CoursesRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbTooltipModule
   ]
 })
 export class CoursesModule { }
