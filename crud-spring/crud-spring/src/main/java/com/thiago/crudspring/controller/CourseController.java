ec57a383ea7ae723c629ee92fe48824da81f9902
Thiago Bessa Rossetto <thiago.rossetto@gpnet.com.br>
2023-03-06 14:01:13 -0300

    SPRING - metodo listar curso por id

--- crud-spring/crud-spring/src/main/java/com/thiago/crudspring/controller/CourseController.java
+++ crud-spring/crud-spring/src/main/java/com/thiago/crudspring/controller/CourseController.java
@@ -3,10 +3,13 @@ package com.thiago.crudspring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +26,15 @@ public class CourseController {
     private CourseRepository courseRepository;
     
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+            .map(course -> ResponseEntity.ok().body(course))
+            .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
