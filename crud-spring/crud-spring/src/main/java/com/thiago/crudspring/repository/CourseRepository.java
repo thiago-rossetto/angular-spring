126e7ad3038731fdc10a2a750c741305f7bd8ecc
Thiago Bessa Rossetto <thiago.rossetto@gpnet.com.br>
2023-03-07 15:17:33 -0300

    SPRING - Update version spring boot

--- crud-spring/crud-spring/src/main/java/com/thiago/crudspring/repository/CourseRepository.java
+++ crud-spring/crud-spring/src/main/java/com/thiago/crudspring/repository/CourseRepository.java
@@ -1,11 +1,9 @@
 package com.thiago.crudspring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.thiago.crudspring.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     
 }
