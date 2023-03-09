72aec72e97bc2f3dbe7a9a25a4434f9819dd8438
Thiago Bessa Rossetto <thiago.rossetto@gpnet.com.br>
2023-03-09 11:31:44 -0300

    Implementndo classe DTO de Curso

---
+++ crud-spring/crud-spring/src/main/java/com/thiago/crudspring/dto/CourseDTO.java
@@ -0,0 +1,14 @@
+package com.thiago.crudspring.dto;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record CourseDTO(
+    Long id,
+    @NotBlank @NotNull @Length(min = 5, max = 100) String name,
+    @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end|Full-stack") String categoria) {
+
+}
